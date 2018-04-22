f337a6f5d5ec9ddd5bf17d97ed8cd2cabe314993
Rebecca Govert <becci.govert@gmail.com>
2018-04-22 18:49:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli = [];
 
 function takeANumber (katzDeliLine, newPersonName) {
   var lineLength = katzDeliLine.length;
-  lineLength++;
+  
   return "Welcome, " + newPersonName + ". You are number " + lineLength + " in line.";
 }
