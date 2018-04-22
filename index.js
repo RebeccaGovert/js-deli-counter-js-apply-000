cde8867e4911ce1f627cd7c083360b75ee9d4aa7
Rebecca Govert <becci.govert@gmail.com>
2018-04-22 19:28:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function nowServing (katzDeliLine) {
     return "There is nobody waiting to be served!"
   }
   firstPerson = katzDeliLine[0];
-  katzDeliLine.shift();
-  return "Currently serving " + firstPerson + ".";
+//  katzDeliLine.shift();
+  return "Currently serving " + katzDeliLine.shift() + ".";
 }
